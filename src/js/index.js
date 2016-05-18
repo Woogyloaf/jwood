b05eeb4f1f7c469b06dffb84846d025296ddeb78
woogyloaf <woogyloaf@gmail.com>
2016-05-18 18:09:27 +0300

    First commit

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+// 'use strict';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './components/app';
+
+// ReactDOM.render(
+//   <App phrase='ES6 React Class Component!'/>,
+//   document.querySelector('#app')
+// );
